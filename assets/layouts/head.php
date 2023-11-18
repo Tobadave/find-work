19d4d86000fccece6a9800e296b4dfec309465c8
NFORSHIFU234 Dev || Shifu-Nfor Nyuiring-yoh Rhagninyui <75176952+nforshifu234dev@users.noreply.github.com>
2023-11-18 02:31:34 +0100

    Added the NFSFU234 Form Validation Library CDN

--- assets/layouts/head.php
+++ assets/layouts/head.php
@@ -2,7 +2,12 @@
 <html lang="en">
 <head>
     <link rel="shortcut icon" href="assets/images/findwork.png" type="image/x-icon">
+    <!-- FONT AWESOME CDN -->
     <script src="assets/js/fontawesome@6.4.0.min.js"></script>
+
+    <!-- NFSFU234 Form Validation CDN -->
+    <script src="http://localhost/@libraries/NFSFU234-FormValidation/dist/js/nfsfu234FormValidation.js"></script>
+    <link rel="stylesheet" href="http://localhost/@libraries/NFSFU234-FormValidation/dist/css/nfsfu234FormValidation.min.css">
 
     <!-- <link rel="stylesheet" href="assets/css/Application_form.css"> -->
     <!-- <link rel="stylesheet" href="assets/css/navbar.css"> -->
