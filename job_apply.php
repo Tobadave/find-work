ce8f4fdf02adbbc183731dd81ef876e24428f42e
NFORSHIFU234 Dev || Shifu-Nfor Nyuiring-yoh Rhagninyui <75176952+nforshifu234dev@users.noreply.github.com>
2023-11-18 15:12:11 +0100

    The Job Apply Page can now use NFSFU234 Form Validation Library

--- job_apply.php
+++ job_apply.php
@@ -82,7 +82,7 @@
 
             <div class="contents">
 
-                <form method="post" action="assets/php/job_apply_logic.php" class="edit-profile-form" >
+                <form method="post" action="assets/php/job_apply_logic.php" class="edit-profile-form" id="job_apply" >
 
                     <section>
                         <div class="heading">
@@ -114,39 +114,39 @@
 
                             <div class="input">
                                 <label for="">Job Title</label>
-                                <input type="text" name="job_title" value="<?php echo $job_info['job_title'] ?>" readonly>
+                                <input type="text" name="job_title" data-attr-name="job_title" value="<?php echo $job_info['job_title'] ?>" readonly>
                             </div>
 
                             <div class="input">
                                 <label for="">Company Name</label>
-                                <input type="text" name="company_name" value="<?php echo fetchUserDetails('employer_id', $job_info['job_author_id'], 'employers' )['company_name']; ?>" readonly>
+                                <input type="text" name="company_name"  data-attr-name="company_name" value="<?php echo fetchUserDetails('employer_id', $job_info['job_author_id'], 'employers' )['company_name']; ?>" readonly>
                             </div>
 
                         </div>
 
                         <div class="input">
                             <label for="">Job Description</label>
-                            <textarea name="job_description" id="" cols="30" rows="10" value="<?php echo $job_info['job_description'] ?>" readonly><?php echo $job_info['job_description'] ?></textarea>
+                            <textarea name="job_description"  data-attr-name="job_desciption"  id="" cols="30" rows="10" value="<?php echo $job_info['job_description'] ?>" readonly><?php echo $job_info['job_description'] ?></textarea>
                         </div>
 
                         <div class="input">
                             <label for="">Salary</label>
-                            <input type="text" name="job_salary" value="<?php echo $job_info['job_salary'] ?>">
+                            <input type="text" name="job_salary"   data-attr-name="job_salary"  value="<?php echo $job_info['job_salary'] ?>">
                         </div>
 
                         <div class="input">
                             <label for="">Required Skills</label>
-                            <input type="text" name="job_skills" value="<?php echo $job_info['job_skills'] ?>">
+                            <input type="text" name="job_skills"  data-attr-name="job_skills"  value="<?php echo $job_info['job_skills'] ?>">
                         </div>
 
                         <div class="input">
                             <label for="">Location</label>
-                            <input type="text" name="location" value="<?php echo $job_info['job_location']; ?>">
+                            <input type="text" name="location"  data-attr-name="location"  value="<?php echo $job_info['job_location']; ?>">
                         </div>
 
                         <div class="input">
                             <label for="">Job Closing Date</label>
-                            <input type="text" name="job_due_date" value="<?php echo $job_info['job_end_date'] ?>" readonly>
+                            <input type="text" name="job_due_date"  data-attr-name="job_due_date"   value="<?php echo $job_info['job_end_date'] ?>" readonly>
                         </div>
                         
                     </section>
@@ -172,47 +172,47 @@
 
                             <div class="input">
                                 <label for="">first Name</label>
-                                <input type="text" name="fname" value="<?php echo $user_info['applicant_first_name'] ?>">
+                                <input type="text" name="fname"   data-attr-name="fname"  value="<?php echo $user_info['applicant_first_name'] ?>">
                             </div>
 
                             <div class="input">
                                 <label for="">last Name</label>
-                                <input type="text" name="lname" value="<?php echo $user_info['applicant_last_name'] ?>">
+                                <input type="text" name="lname"  data-attr-name="lname"  value="<?php echo $user_info['applicant_last_name'] ?>">
                             </div>
 
                         </div>
 
                         <div class="input">
                             <label for="">Email</label>
-                            <input type="email" name="email" value="<?php echo $user_info['applicant_email'] ?>">
+                            <input type="email" name="email"  data-attr-name="email"  value="<?php echo $user_info['applicant_email'] ?>">
                         </div>
 
                         <div class="input">
                             <label for="">Phone Number</label>
-                            <input type="text" name="email" value="<?php echo $user_info['applicant_phone_number'] ?>">
+                            <input type="text" name="phone"  data-attr-name="phone"  value="<?php echo $user_info['applicant_phone_number'] ?>">
                         </div>
 
                         <div class="input">
                             <label for="">My Skills</label>
-                            <input type="text" name="email" value="<?php echo $user_info['applicant_skills'] ?>">
+                            <input type="text" name="skills"  data-attr-name="skills"  value="<?php echo $user_info['applicant_skills'] ?>">
                         </div>
 
                         <div class="input">
                             <label for="">My Education History</label>
-                            <input type="text" name="email" value="<?php echo $user_info['applicant_education_history'] ?>">
+                            <input type="text" name="education"  data-attr-name="education"  value="<?php echo $user_info['applicant_education_history'] ?>">
                         </div>
 
                         <div class="input">
                             <label for="">Resume Link</label>
-                            <input type="url" name="resume_url" value="<?php echo $user_info['applicant_resume_url'] ?>">
+                            <input type="url" name="resume_url"  data-attr-name="resume_url"  value="<?php echo $user_info['applicant_resume_url'] ?>">
                         </div>
                         
                     </section>
 
                     <section class="main">
-                        <input type="hidden" name="job_id" value="<?php echo $job_info['job_id'] ?>">
-                        <input type="hidden" name="applicant_id" value="<?php echo fetchUserDetails('id', $_SESSION['id'])['id'] ?>">
-                        <input type="hidden" name="url" value="<?php echo getCurrentPageURL(); ?>">
+                        <input type="hidden" name="job_id"  data-attr-name="job_id"  value="<?php echo $job_info['job_id'] ?>" required>
+                        <input type="hidden" name="applicant_id"  data-attr-name="applicant_id"  value="<?php echo fetchUserDetails('id', $_SESSION['id'])['id'] ?>" required>
+                        <input type="hidden" name="url"  data-attr-name="url"  value="<?php echo getCurrentPageURL(); ?>" required>
                     </section>
 
                     <div class="input">
@@ -231,6 +231,77 @@
         document.querySelectorAll('input').forEach(input => {
             input.setAttribute('readonly', '');
         });
+    </script>
+
+    <script>
+
+        const form = document.getElementById('job_apply');
+
+        // Example form details object
+        const formDetails = {
+            form: form,
+            isErrorMessageInline: true,
+        };
+
+        // Example AJAX options object
+        const ajaxOptions = {
+            url: "assets/php/job_apply_logic.php",
+            RequestMethod: "POST",
+            RequestHeader: {
+                "Content-Type": "application/json",
+            },
+        };
+
+        // Forge a customized NFSFU234 Form Validation instance
+        const formValidator = new NFSFU234FormValidation(formDetails, ajaxOptions);
+
+        formValidator.submit();
+
+        const submitBtn = form.querySelector('button');
+        submitBtn.addEventListener('click',  ()=>{
+
+            let responsePromise = formValidator.getAJAXResponse();
+
+            responsePromise 
+                .then((response) => {
+                // Success: Server response received in JSON format
+                // console.log('Request successful', response);
+
+                if( response.status !== 200 )
+                {
+                    errorDetails = {
+                        type : 'modal',
+                        message: response.message,
+                        duration: 3000,
+                        element: form,
+                        success: false,
+                    }
+                }
+                else
+                {
+                    errorDetails = {
+                        type : 'modal',
+                        message: response.message,
+                        duration: 3000,
+                        element: form,
+                        success: true,
+                    }
+                    
+
+                    setTimeout(() => {
+                        location.reload();
+                    }, 5000);
+
+                }
+
+                formValidator.displayError(errorDetails);
+
+            });
+
+            
+
+        });
+
     </script>
 
 </body>
